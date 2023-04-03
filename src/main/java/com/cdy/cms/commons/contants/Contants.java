d98424495c0d7eba0cf84fb618fb15abad1be935
Violet <3104044743@qq.com>
2023-04-03 19:38:31 +0800

    变更4

--- src/main/java/com/cdy/cms/commons/contants/Contants.java
+++ src/main/java/com/cdy/cms/commons/contants/Contants.java
@@ -3,5 +3,5 @@ package com.cdy.cms.commons.contants;
 public class Contants {
     public static final String RETURN_OBJECT_CODE_SUCCESS = "1";
     public static final String RETURN_OBJECT_CODE_FAIL = "0";
-    public static final String SESSION_USER = "sessionUser";
+    public static final String SESSION_TEACHER = "sessionTeacher";
 }
