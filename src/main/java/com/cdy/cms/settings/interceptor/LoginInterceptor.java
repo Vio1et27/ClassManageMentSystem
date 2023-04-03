75b362ca0aeab456397e7e2ae66af99ee7d8c868
Violet <3104044743@qq.com>
2023-04-03 19:15:49 +0800

    变更4

--- src/main/java/com/cdy/cms/settings/interceptor/LoginInterceptor.java
+++ src/main/java/com/cdy/cms/settings/interceptor/LoginInterceptor.java
@@ -1,9 +1,35 @@
 package com.cdy.cms.settings.interceptor;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  * 放置拦截器，防止用户在不登录情况下通过controller任意跳转到别的界面
  *
  */
-public class LoginInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        /*HttpSession session = httpServletRequest.getSession();
+        if ((User)session.getAttribute(Contants.SESSION_USER) == null) {
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath());
+            return false;
+        }
+        return true;*/
+        return true;
+    }
 
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
 }
--- src/main/webapp/test.jsp
+++ src/main/webapp/test.jsp
@@ -4,7 +4,6 @@
     <title>Title</title>
 </head>
 <body>
-    <!--这里放登录页面-->
-这是一个登录界面
+
 </body>
 </html>
